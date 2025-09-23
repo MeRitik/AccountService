84c8eff51425b5f26465989fe5b973678216cc35
Ritik Ranjan <ritikranjan999@gmail.com>
2025-09-23 19:21:25 +0530

    Initialize Spring Boot project with account service setup
    Implemented a new Spring Boot project for managing accounts. Added repository and entity classes.

---
+++ src/main/java/com/ritik/accountservice/repository/AccountsRepository.java
@@ -0,0 +1,9 @@
+package com.ritik.accountservice.repository;
+
+import com.ritik.accountservice.entity.Accounts;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AccountsRepository extends JpaRepository<Accounts, Long> {
+}
---
+++ src/main/java/com/ritik/accountservice/repository/CustomerRepository.java
@@ -0,0 +1,9 @@
+package com.ritik.accountservice.repository;
+
+import com.ritik.accountservice.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+}
