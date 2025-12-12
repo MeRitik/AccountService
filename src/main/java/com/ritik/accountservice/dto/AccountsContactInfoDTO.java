1907f821c7357b6afab71db6b425c1d70ad93ceb
Ritik Ranjan <ritikranjan999@gmail.com>
2025-12-12 17:55:44 +0530

    Update `AccountsContactInfoDTO` prefix, add environment-specific YAML configurations, and update build version

--- src/main/java/com/ritik/accountservice/dto/AccountsContactInfoDTO.java
+++ src/main/java/com/ritik/accountservice/dto/AccountsContactInfoDTO.java
@@ -5,7 +5,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import java.util.List;
 import java.util.Map;
 
-@ConfigurationProperties(prefix = "accounts")
+@ConfigurationProperties(prefix = "cards")
 public record AccountsContactInfoDTO(
         String message,
         Map<String, String> contactDetails,
